94de716ecc2c4ed2b3aed6eb8699beeed81570f4
Maria Frolova <m89067536142@yandex.ru>
2024-10-17 13:16:00 +0300

    git commit -m "add Diplom_1" git commit -m "add Diplom_1_v.2" git commit -m "add index.html" git commit -m "add Diplom_1_v.3"

--- tests/test_buns.py
+++ tests/test_buns.py
@@ -1,10 +1,15 @@
 from praktikum.bun import Bun
 from data import bun_price, bun_name
 class TestBuns:
-    def test_get_bun(self):
+    def test_get_bun_name(self):
         bun = Bun(bun_name, bun_price)
-        bun.get_name()
-        bun.get_price()
+
         assert bun.name == bun_name
+
+
+    def test_get_bun_price(self):
+        bun = Bun(bun_name, bun_price)
+
+        bun.get_price()
         assert bun.price == bun_price
 
--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -6,18 +6,24 @@ from data import ingredient_type_first, ingredient_name_first, ingredient_price_
 class TestIngredients:
 
 
-    def test_get_ingredient(self):
+    def test_get_ingredient_type(self):
         ingredient = Ingredient(ingredient_type_first, ingredient_name_first, ingredient_price_first)
         ingredient.get_type()
-        ingredient.get_name()
-        ingredient.get_price()
+
         assert ingredient.type == ingredient_type_first
+
+
+    def test_get_ingredient_name(self):
+        ingredient = Ingredient(ingredient_type_first, ingredient_name_first, ingredient_price_first)
+        ingredient.get_name()
+
         assert ingredient.name == ingredient_name_first
+
+
+    def test_get_ingredient_price(self):
+        ingredient = Ingredient(ingredient_type_first, ingredient_name_first, ingredient_price_first)
+        ingredient.get_price()
+
         assert ingredient.price == ingredient_price_first
-
-
-
-
-
 
 
