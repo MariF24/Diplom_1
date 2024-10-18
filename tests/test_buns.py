93e4035a8c6d2081513283305c531c1cb4da9415
Maria Frolova <m89067536142@yandex.ru>
2024-10-18 11:28:29 +0300

    git commit -m "add Diplom_1" git commit -m "add Diplom_1_v.2" git commit -m "add index.html" git commit -m "add Diplom_1_v.3" git commit -m "add Diplom_1_v.3_2" git commit -m "add Diplom_1_v.4"

--- tests/test_buns.py
+++ tests/test_buns.py
@@ -3,14 +3,12 @@ from data import bun_price, bun_name
 class TestBuns:
     def test_get_bun_name(self):
         bun = Bun(bun_name, bun_price)
-        bun.get_name()
 
-        assert bun.name == bun_name
+        assert bun.get_name() == bun_name
 
 
     def test_get_bun_price(self):
         bun = Bun(bun_name, bun_price)
-        bun.get_price()
 
-        assert bun.price == bun_price
+        assert bun.get_price() == bun_price
 
