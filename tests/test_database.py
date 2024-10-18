db39712df06b6f1fed5894e42f0b89af357a1532
Maria Frolova <m89067536142@yandex.ru>
2024-10-18 20:36:49 +0300

    git commit -m "add Diplom_1" git commit -m "add Diplom_1_v.2" git commit -m "add index.html" git commit -m "add Diplom_1_v.3" git commit -m "add Diplom_1_v.3_2" git commit -m "add Diplom_1_v.4" git commit -m "add Diplom_1_v.5" git commit -m "add Diplom_1_v.6"

--- tests/test_database.py
+++ tests/test_database.py
@@ -5,10 +5,10 @@ class TestDatabases:
 
     def test_available_buns_true(self):
         database = Database()
-        database.available_buns()
+
         assert database.available_buns() == database.buns
 
     def test_available_ingredients_true(self):
         database = Database()
-        database.available_ingredients()
+
         assert database.available_ingredients() == database.ingredients
--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -8,21 +8,18 @@ class TestIngredients:
 
     def test_get_ingredient_type(self):
         ingredient = Ingredient(ingredient_type_first, ingredient_name_first, ingredient_price_first)
-        ingredient.type = ingredient.get_type()
 
-        assert ingredient.type == ingredient_type_first
+        assert ingredient.get_type() == ingredient_type_first
 
     def test_get_ingredient_name(self):
         ingredient = Ingredient(ingredient_type_first, ingredient_name_first, ingredient_price_first)
-        ingredient.name = ingredient.get_name()
 
-        assert ingredient.name == ingredient_name_first
+        assert ingredient.get_name() == ingredient_name_first
 
 
     def test_get_ingredient_price(self):
         ingredient = Ingredient(ingredient_type_first, ingredient_name_first, ingredient_price_first)
-        ingredient.price = ingredient.get_price()
 
-        assert ingredient.price == ingredient_price_first
+        assert ingredient.get_price() == ingredient_price_first
 
 
